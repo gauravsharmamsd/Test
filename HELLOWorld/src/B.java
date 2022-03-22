da1ee994a3716f8a93435e8afcbae33a8ae8e595
Gaurav Kaushik <gauravsharmamsd@gmail.com>
2022-03-22 01:28:13 -0700

    change2

--- HELLOWorld/src/B.java
+++ HELLOWorld/src/B.java
@@ -1,7 +1,6 @@
 
 public class B {
 public static void main(String[] args) {
-	System.out.println("hii.....");
-	System.out.println("change in gitub.....");
-}
+	System.out.println("B added");
+	
 }
